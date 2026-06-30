5f156ae1f591325de9d0dcba99885fd0707fefaf
desyLoyz <alois.georg.wieshuber@desy.de>
2026-06-30 18:05:54 +0200

    fix: correct base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/ric-cm-nav/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/ric-cm-docs/' : '/',
   plugins: [
     vue(),
   ],
