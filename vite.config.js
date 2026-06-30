4fe8f20e11555c4c8a1692569ec79b2a346b3e8d
desyLoyz <alois.georg.wieshuber@desy.de>
2026-06-30 18:21:18 +0200

    fix: revert base path to /ric-cm-nav/

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/ric-cm-docs/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/ric-cm-nav/' : '/',
   plugins: [
     vue(),
   ],
